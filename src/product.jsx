b28396042eb1c255d1bc9828f8209f96d5b3057c
ezeiboboi <technerdworld01@gmail.com>
2023-07-08 10:47:25 +0100

    first commit

---
+++ src/product.jsx
@@ -0,0 +1,9 @@
+const product = {
+    name: "Product Name",
+    price: "$10",
+    description: "Product description goes here",
+    imageUrl: "src/assets/images/aic-home.jpg" // Replace "your_product_image_url" with the URL of the product image
+  };
+  
+  export default product;
+  
